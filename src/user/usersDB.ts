c5d9a1815e21268ec45d7b9c22e46eb1aaf8771c
Igor Sergiienko <igor_sergiienko@epam.com>
2020-01-29 16:15:38 +0700

    Add lint and readme

--- src/user/usersDB.ts
+++ src/user/usersDB.ts
@@ -1,42 +1,42 @@
-import * as uuid from 'uuid/v1'
-import { User } from './user.interface'
-import { initialUsers } from './data/index'
+import * as uuid from 'uuid/v1';
+import { User } from './user.interface';
+import { initialUsers } from './data/index';
 
-let users: User[] = [...initialUsers]
+let users: User[] = [...initialUsers];
 
 export const userDB = {
-  get: (id: string): User | undefined => {
-    return users.find(user => user.id === id)
-  },
-  getByLogin: (loginSubstring: string, limit: number = 10): User[] => {
-    return users
-      .filter(user => user.login.includes(loginSubstring))
-      .slice(0, limit)
-  },
-  create: (user: Partial<User>): User => {
-    const newUser = { ...user, id: uuid(), isDeleted: false } as User
+    get: (id: string): User | undefined => {
+        return users.find(user => user.id === id);
+    },
+    getByLogin: (loginSubstring: string, limit: number = 10): User[] => {
+        return users
+            .filter(user => user.login.includes(loginSubstring))
+            .slice(0, limit);
+    },
+    create: (user: Partial<User>): User => {
+        const newUser = { ...user, id: uuid(), isDeleted: false } as User;
 
-    users = [
-      ...users,
-      newUser,
-    ]
+        users = [
+            ...users,
+            newUser
+        ];
 
-    return newUser
-  },
-  update: (id: string, updatedUser: Partial<User>): User => {
-    users = users.map(user => id === user.id ? {
-      ...user,
-      ...updatedUser,
-    } : user)
+        return newUser;
+    },
+    update: (id: string, updatedUser: Partial<User>): User => {
+        users = users.map(user => id === user.id ? {
+            ...user,
+            ...updatedUser
+        } : user);
 
-    return userDB.get(id)
-  },
-  remove: (id: string): boolean => {
-    users = users.map(user => ({
-      ...user,
-      isDeleted: id === user.id || user.isDeleted
-    }))
+        return userDB.get(id);
+    },
+    remove: (id: string): boolean => {
+        users = users.map(user => ({
+            ...user,
+            isDeleted: id === user.id || user.isDeleted
+        }));
 
-    return !!userDB.get(id)
-  },
-}
+        return !!userDB.get(id);
+    }
+};
--- src/user/validate.ts
+++ src/user/validate.ts
@@ -1,33 +1,33 @@
-import * as Joi from '@hapi/joi'
+import * as Joi from '@hapi/joi';
 
 const login = Joi.string()
-  .alphanum()
-  .min(3)
-  .max(20)
+    .alphanum()
+    .min(3)
+    .max(20);
 
 const password = Joi.string()
-  .regex(
-    /^(?=.*[a-zA-Z])(?=.*[0-9])/,
-    'at least one letter and one number'
-  )
-  .min(8)
-  .max(20)
+    .regex(
+        /^(?=.*[a-zA-Z])(?=.*[0-9])/,
+        'at least one letter and one number'
+    )
+    .min(8)
+    .max(20);
 
 const age = Joi.number()
-  .min(4)
-  .max(130)
+    .min(4)
+    .max(130);
 
 const schemaOnCreate = Joi.object({
-  login: login.required(),
-  password: password.required(),
-  age: age.required(),
-})
+    login: login.required(),
+    password: password.required(),
+    age: age.required()
+});
 
 const schemaOnUpdate = Joi.object({
-  login,
-  password,
-  age,
-})
+    login,
+    password,
+    age
+});
 
-export const validateOnUpdate = (user) => schemaOnUpdate.validate(user)
-export const validateOnCreate = (user) => schemaOnCreate.validate(user)
+export const validateOnUpdate = (user) => schemaOnUpdate.validate(user);
+export const validateOnCreate = (user) => schemaOnCreate.validate(user);
