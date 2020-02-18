87dc36f9dfa7dbdec4b8298c94e45b5d188e1bfb
igor0ser <igor0ser@gmail.com>
2020-02-18 16:30:06 +0200

    Make eslint check all files and fix them

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import * as express from 'express'
-import * as bodyParser from 'body-parser'
-import { userRouter } from './user/router'
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import { userRouter } from './user/router';
 
-const app = express()
+const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.send('Node.js homework #2')
-})
+app.get('/', (req, res) => {
+    res.send('Node.js homework #2');
+});
 
-app.use('/users', userRouter)
+app.use('/users', userRouter);
 
-app.listen(3000)
+app.listen(3000);
 
-console.log('App is listening on port 3000')
+console.log('App is listening on port 3000');
