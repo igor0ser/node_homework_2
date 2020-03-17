913b8cb8a844eaf11f1d0ff279470b224182d37e
Igor Sergiienko <igor_sergiienko@epam.com>
2020-03-17 18:36:13 +0200

    Added id to User in migration

--- database/models/user.js
+++ database/models/user.js
@@ -1,13 +1,18 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
     login: DataTypes.STRING,
     password: DataTypes.STRING,
     age: DataTypes.INTEGER,
     isDeleted: DataTypes.BOOLEAN
   }, {});
   User.associate = function(models) {
-    // associations can be defined here
   };
   return User;
 };
